39f5a75f796a45bdf35311038b2d75a6f71b3599
mojouy <santiago.mollajoli@neonroots.com>
2015-09-22 19:54:13 -0300

    Movement code.

--- Assets/Scripts/Truck/TruckController.cs
+++ Assets/Scripts/Truck/TruckController.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class TruckController : MonoBehaviour {
 
-	public GameObject bonus;
-
 	//Handling Car
-	public float standingThreshold = 85;
+	public float tiltThreshold = 85;
 	public float Acceleration = 10f;
 	[HideInInspector] public Vector3 speed;
-	private float TurnAcceleration = 120f;
+	private float TurnAcceleration = 150f;
 	[SerializeField] private Vector3 direction;
 	private bool turning;
+	private bool isStanding;
+	private bool notFlying;
+	private bool onGround;
+	private float flyingTime = 2f;
 
 	//Street Data
 	private StreetData currentStreet;
@@ -19,48 +21,66 @@ public class TruckController : MonoBehaviour {
 	void Start () {
 		direction = Vector3.forward;
 		turning = false;
+		isStanding = true;
+		notFlying = false;
+		onGround = false;
 	}
 	
 	void Update () {
+		IsStanding();
+
 		speed *= 0.8f;
 		
-		if (Input.GetKey (KeyCode.UpArrow)) {
+		if (isStanding && onGround) {
 			speed += Acceleration * direction;
-		}
-		
-		//right
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.Rotate(Vector3.up, TurnAcceleration * Time.deltaTime);
-		}
 
-		//left
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.Rotate(Vector3.up, -TurnAcceleration * Time.deltaTime);
-		}
+			//right
+			if (Input.GetKey(KeyCode.RightArrow)) {
+				transform.Rotate(Vector3.up, TurnAcceleration * Time.deltaTime);
+			}
+			
+			//left
+			if (Input.GetKey(KeyCode.LeftArrow)) {
+				transform.Rotate(Vector3.up, -TurnAcceleration * Time.deltaTime);
+			}
 
-		transform.position += speed * Time.deltaTime;
+			transform.position += speed * Time.deltaTime;
+		}//if standing and not flipped
 		
 		direction = transform.forward;
 		direction.y = 0;
-
-		print (IsStanding());
 	}
 
-	public bool IsStanding () {
+	public void IsStanding () {
 		Vector3 carAngle = transform.eulerAngles;
 		float tiltX = Mathf.Abs (carAngle.x);
 		float tiltZ = Mathf.Abs (carAngle.z);
-		
-		if (tiltX > standingThreshold || tiltZ > standingThreshold) {
-			return false;
+		float oppositeAngle = 360 - tiltThreshold;
+
+		isStanding = tiltX > tiltThreshold && tiltX < oppositeAngle || tiltZ > tiltThreshold && tiltZ < oppositeAngle ? false : true;
+	}
+
+	void OnCollisionStay (Collision coll) {
+		if (coll.gameObject.tag == "ground") {
+			notFlying = true;
+			onGround = true;
+			print ("I'm Riding !");
 		}
-		return true;
 	}
 
-
-	void OnTriggerEnter(Collider other){
+	void OnCollisionExit (Collision coll) {
+		if (coll.gameObject.tag == "ground" && onGround) {
+			Invoke ("KeepsFlying", flyingTime);
+		}
 	}
-	
-	void OnTriggerExit(Collider other){
+
+	public void KeepsFlying () {
+		onGround = false;
+		notFlying = false;
+		print ("Psss.. Master? I keep Flying");
 	}
 }
+
+
+
+
