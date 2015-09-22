18d6c35242435cf25f433e2ab43fc94cd28ed1e2
mojouy <santiago.mollajoli@neonroots.com>
2015-09-22 12:38:08 -0300

    Fix movement, remove unnecesary content and add level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+	
+	void Start () {
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log("auto load next level disabled, use a posisitve number in seconds");
+		} else {
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+	
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
+	
--- Assets/Scripts/Truck/TruckController.cs
+++ Assets/Scripts/Truck/TruckController.cs
@@ -2,123 +2,65 @@
 using System.Collections;
 
 public class TruckController : MonoBehaviour {
-	public float Acceleration = 10;
-	[HideInInspector] public Vector3 Speed;
-	public float TurnAcceleration = 10;
-	public float TurnSpeed = 0;
-	
-	private StreetData currentStreet;
-	private bool N, W, E, S;
+
+	public GameObject bonus;
+
+	//Handling Car
+	public float standingThreshold;
+	public float Acceleration = 10f;
+	[HideInInspector] public Vector3 speed;
+	private float TurnAcceleration = 120f;
 	[SerializeField] private Vector3 direction;
-	[SerializeField] private Quaternion rotation;
 	private bool turning;
-	private CrossOptions currentOptions;
+
+	//Street Data
+	private StreetData currentStreet;
 
 	void Start () {
 		direction = Vector3.forward;
 		turning = false;
-		SetCardinalDirection ();
 	}
 	
 	void Update () {
-		rotation = transform.rotation;
-		Speed *= 0.8f;
-		TurnSpeed *= 0.8f;
+		speed *= 0.8f;
 		
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			Speed += Acceleration * direction;
+			speed += Acceleration * direction;
+			print (speed);
 		}
 		
 		//right
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			TurnSpeed -= TurnAcceleration;
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Rotate(Vector3.up, TurnAcceleration * Time.deltaTime);
 		}
+
 		//left
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			TurnSpeed += TurnAcceleration;
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Rotate(Vector3.up, -TurnAcceleration * Time.deltaTime);
 		}
-		
-		rotation.y -= TurnSpeed * Time.deltaTime;
-		transform.rotation = rotation;
-		transform.position += Speed * Time.deltaTime;
+
+		transform.position += speed * Time.deltaTime;
 		
 		direction = transform.forward;
+		direction.y = 0;
 	}
 
-	void SetCardinalDirection () {
-		E = direction.z == 1;
-		W = direction.z == -1;
-		S = direction.x == 1;
-		N = direction.x == -1;
-	}
-
-	bool CanTurn(bool right) {
-		if (currentOptions != null) {
-			return right && 
-					(E && currentOptions.TurningOptions.S) ||
-					(N && currentOptions.TurningOptions.E) ||
-					(W && currentOptions.TurningOptions.N) ||
-					(S && currentOptions.TurningOptions.W) ||
-				  !right &&
-					(E && currentOptions.TurningOptions.N) ||
-					(N && currentOptions.TurningOptions.W) ||
-					(W && currentOptions.TurningOptions.S) ||
-					(S && currentOptions.TurningOptions.E);
+	public bool IsStanding () {
+		Vector3 pinAngle = transform.eulerAngles;
+		float tiltX = Mathf.Abs (pinAngle.x);
+		float tiltZ = Mathf.Abs (pinAngle.z);
+		
+		if (tiltX > standingThreshold || tiltZ > standingThreshold) {
+			return false;
 		}
-		return false;
+		return true;
 	}
+
 
 	void OnTriggerEnter(Collider other){
-		if (other.gameObject.tag.Equals("Corner")) {
-			currentOptions = other.GetComponent<CrossOptions> ();			
-		}
+
 	}
 	
 	void OnTriggerEexit(Collider other){
-		if (other.gameObject.tag.Equals("Corner")) {
-			currentOptions = null;			
-		}
 	}
-	
-	Vector3 TurnFinalPosition(Vector3 targetDirection) {
-		if (currentOptions != null) {
-			Vector3 targetPosition = currentOptions.gameObject.transform.position;
-			targetPosition.x += targetDirection.x * 10f;
-			targetPosition.z += targetDirection.z * 10f;
-			targetPosition.y = 0;
-			return targetPosition;
-		}
-
-		return transform.position;
-	}
-
-//	IEnumerator Turn(float angle) {
-//		Quaternion rotation = Quaternion.Euler (0, angle, 0);
-//		
-//		Quaternion targetOrientation = transform.rotation * rotation;
-//		Vector3 targetDirection = (rotation * direction).normalized;
-//		Vector3 targetPosition = TurnFinalPosition(targetDirection);
-//		
-//		float startTime = Time.time;
-//		float scale = TurnSpeedScale;
-//		float turnTime = TurnTime;
-//		float delta = 0;
-//		
-//		while (Time.time - startTime < turnTime) {
-//			delta = (Time.time - startTime) / turnTime;
-//
-//			transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, targetOrientation, delta);
-//			direction = Vector3.Slerp(direction, targetDirection, delta);
-//			transform.position = Vector3.Slerp(transform.position, targetPosition, delta);
-//			
-//			yield return new WaitForSeconds(0.1f);
-//		}
-//		transform.rotation = targetOrientation;
-//		direction = targetDirection.normalized;
-//		transform.position = targetPosition;
-//		turning = false;
-//		currentOptions = null;
-//		SetCardinalDirection ();
-//	}
-
 }
