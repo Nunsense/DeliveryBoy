7db176baa5c5eb8683380477f3d3779bd346c096
mojouy <santiago.mollajoli@neonroots.com>
2015-12-18 14:47:24 -0300

    Remove un-used vars

--- Assets/Scripts/CarRespawn.cs
+++ Assets/Scripts/CarRespawn.cs
@@ -6,7 +6,6 @@ public class CarRespawn : MonoBehaviour {
 	// Use this for initialization
 	private int counter;
 	private GameObject[] streets;
-	private Vector3 lastPosition, lastPlayerPosition;
 	private float collisionDistance = 0.1f;
 
 	public int maxStuck = 100;
@@ -15,8 +14,6 @@ public class CarRespawn : MonoBehaviour {
 	void Awake () {
 		counter = 0;
 		streets = GameObject.FindGameObjectsWithTag ("Street");
-		lastPosition = transform.position;
-		lastPlayerPosition = player.position;
 	}
 		
 	void OnCollisionStay (Collision other) {
@@ -27,16 +24,9 @@ public class CarRespawn : MonoBehaviour {
 	}//collision stay
 		
 	void ResetPosition () {
-		
-//		gam newPos = new Vector3 ();
-
 		Transform newPos = streets [Random.Range (0, (streets.Length - 1))].transform;
 		transform.position = new Vector3(newPos.position.x, transform.position.y, newPos.position.z);
 		transform.rotation = Quaternion.LookRotation (newPos.right);
 		counter = 0;
-
-		//reset positions
-		lastPosition = transform.position;
-		lastPlayerPosition = player.position;
 	}//reset Position
 }
