843ee5ff1600edce09b08dc052b75d5aa989d5e2
Iuval <iuvalgoldansky@gmail.com>
2015-12-27 15:05:03 -0300

    Refactor map to use grid instead of rays to move

--- Assets/Scripts/CarRespawn.cs
+++ Assets/Scripts/CarRespawn.cs
@@ -8,12 +8,26 @@ public class CarRespawn : MonoBehaviour {
 	private GameObject[] streets;
 	private float collisionDistance = 0.1f;
 
+	CityManager city;
+
 	public int maxStuck = 100;
 	public Transform player;
 
 	void Awake () {
 		counter = 0;
-		streets = GameObject.FindGameObjectsWithTag ("Street");
+		city = GameObject.FindObjectOfType<CityManager>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		GetComponentInChildren<UnityStandardAssets.Vehicles.Car.CarAIControl>().SetTarget(player);
+	}
+
+	void Start() {
+		ResetPosition ();
+	}
+
+	void Update() {
+		if (transform.position.y < -1.5) {
+			ResetPosition ();
+		}
 	}
 		
 	void OnCollisionStay (Collision other) {
@@ -23,10 +37,11 @@ public class CarRespawn : MonoBehaviour {
 		} else  { counter = 0; }
 	}//collision stay
 		
-	void ResetPosition () {
-		Transform newPos = streets [Random.Range (0, (streets.Length - 1))].transform;
-		transform.position = new Vector3(newPos.position.x, transform.position.y, newPos.position.z);
-		transform.rotation = Quaternion.LookRotation (newPos.right);
+	public void ResetPosition () {
+		Vector3 newPos = city.PositionNearPlayer();
+//		Debug.Log(newPos);
+		transform.position = newPos;
+		transform.rotation = Quaternion.LookRotation (Vector3.forward);
 		counter = 0;
 	}//reset Position
 }
