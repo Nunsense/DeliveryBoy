ae7c3ef6b61b13a2b5948ddb6dc32fc84e88bbf3
jjszolno <jjszolno@gmail.com>
2016-01-07 16:24:34 -0300

    Added time challenges.

--- Assets/Scripts/ObjectiveController.cs
+++ Assets/Scripts/ObjectiveController.cs
@@ -6,26 +6,52 @@ public class ObjectiveController : MonoBehaviour {
 
 	private int points;
 	public Text pointsField;
+	public Text timeField;
+	public float timeFactor = 0.2f;
+	private bool status = true;
+	private GameObject cube, circle;
+	private Vector3 lastPosition;
+	private float time;
 	// Use this for initialization
 	void Start () {
 		points = 0;
+		circle = transform.Find("Circle").gameObject;
+		circle.SetActive (false);
+		cube = transform.Find("Cube").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!status) {
+			time -= Time.deltaTime;
+			timeField.text = (time<0) ?
+				"-" + (int)(time / 60) + ":" + Mathf.Abs((int)(time % 60)) :
+				"" + (int)(time / 60) + ":" + Mathf.Abs((int)(time % 60));
+		}
 	}
 
 	void OnTriggerEnter(Collider collision) {
-		//Application.LoadLevel (0);
-		points ++;
-		pointsField.text = "" + points;
-		PlayerPrefs.SetInt("score", points);
-		print ("Points: " + points);
-		Relocate ();
+		if(collision.gameObject.tag.Equals("Player")){
+			Relocate ();
+			if (status) {
+				timeField.gameObject.SetActive (true);
+				cube.SetActive (false);
+				circle.SetActive (true);
+				time = Vector3.Distance (lastPosition, transform.position) * timeFactor;
+			} else {
+				timeField.gameObject.SetActive (false);
+				points = points + (int)time;
+				pointsField.text = "" + points;
+				PlayerPrefs.SetInt ("score", points);
+				circle.SetActive (false);
+				cube.SetActive (true);
+			}
+			status = !status;
+		}
 	}
 
 	public void Relocate(){
+		lastPosition = transform.position;
 		GameObject[] corners = GameObject.FindGameObjectsWithTag("Corner");
 		var index = Random.Range (0, (corners.Length - 1));
 		var position = corners [index].transform.position;
